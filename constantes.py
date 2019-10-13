5dfd22bb0ccb57036671c5f0cfe1275071d1d282
Eric Sérandour <internet-1@serandour.org>
2019-10-13 14:46:06 +0200

    Create constantes.py

---
+++ constantes.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+########################################################################
+# CHOIX DE L'AFFICHAGE
+########################################################################
+
+MODE_GRAPHIQUE = True  # True : Pour afficher la grille dans une fenêtre et dans la console
+                       # False : Pour afficher la grille dans la console exclusivement
+TEMPS_CHUTE = 0.1  # 0.1 pour visualiser la chute des pions
+
+
+
+
+########################################################################
+# REGLAGES POUR LA COMPETITION
+########################################################################
+
+NB_PARTIES = 6  # A adapter (un nombre pair plus grand que 1)
+IA_JAUNE = 2  # Numéro de l'IA en compétition (entre 0 et 9 actuellement)
+IA_ROUGE = 2  # Numéro de l'IA en compétition (entre 0 et 9 actuellement)
+
+########################################################################
+
+
+
+
+########################################################################
+# TAILLE DE LA GRILLE
+########################################################################
+
+NB_COLONNES = 7  # A adapter
+NB_LIGNES = 6  # A adapter
+
+########################################################################
