01e97b716521e062788a5857b9ee553efce0b198
Eric Sérandour <internet-1@serandour.org>
2019-10-13 16:24:31 +0200

    Update P4_ordinateur_ordinateur.py

--- P4_ordinateur_ordinateur.py
+++ P4_ordinateur_ordinateur.py
@@ -5,7 +5,7 @@
 #  Puissance 4 - Ordinateur contre Ordinateur avec IA
 #
 #  Copyright 2016-2019 - Eric Sérandour
-#  Version du 13 octobre 2019 à 15 h 43
+#  Version du 13 octobre 2019 à 16 h 29
 #
 #  This program is free software; you can redistribute it and/or
 #  modify it under the terms of the GNU General Public License as
@@ -142,7 +142,7 @@ def competition(couleur, ia1, ia2):
 ########################################################################
 
 if NB_PARTIES > 1:
-    competition('yellow', IA_JAUNE, IA_ROUGE)  # Les jaunes commencent
-    competition('red', IA_ROUGE, IA_JAUNE)  # Les rouges commencent
+    competition('yellow', IA_JAUNE, IA_ROUGE)  # Les jaunes commencent sur la moitié des parties
+    competition('red', IA_ROUGE, IA_JAUNE)  # Les rouges commencent sur la moitié des parties
 
 ########################################################################
