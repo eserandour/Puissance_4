c4ab951b7fe9991dbbcfac962d3405c88de6a4f1
Eric Sérandour <internet-1@serandour.org>
2019-10-13 21:27:28 +0200

    Update constantes.py

--- constantes.py
+++ constantes.py
@@ -1,9 +1,24 @@
 # -*- coding: utf-8 -*-
 """
 ########################################################################
-#  Version du 13 octobre 2019 à 19 h 55
+#  Version du 13 octobre 2019 à 21 h 30
 ########################################################################
 """
+
+########################################################################
+# REGLAGES POUR LA COMPETITION
+########################################################################
+
+NB_PARTIES = 6  # A adapter (un nombre pair plus grand que 1)
+# Pour Ordinateur contre Ordinateur : IA_JAUNE et IA_ROUGE
+# Pour Humain contre Ordinateur : IA_ROUGE
+IA_JAUNE = 8  # Numéro de l'IA en compétition (entre 0 et 9 actuellement)
+IA_ROUGE = 5  # Numéro de l'IA en compétition (entre 0 et 9 actuellement)
+
+########################################################################
+
+
+
 
 ########################################################################
 # CHOIX DE L'AFFICHAGE
@@ -11,22 +26,9 @@
 
 MODE_GRAPHIQUE = True  # True : Pour afficher la grille dans une fenêtre et dans la console
                        # False : Pour afficher la grille dans la console exclusivement
-TEMPS_CHUTE = 0  # 0.1 pour visualiser la chute des pions
+TEMPS_CHUTE = 0.1  # 0.1 pour visualiser la chute des pions sinon 0
 LARGEUR_GRILLE = 480  # A adapter
 ESPACEMENT = LARGEUR_GRILLE / 64  # Espace entre 2 trous de la grille
-
-########################################################################
-
-
-
-
-########################################################################
-# REGLAGES POUR LA COMPETITION
-########################################################################
-
-NB_PARTIES = 6  # A adapter (un nombre pair plus grand que 1)
-IA_JAUNE = 1  # Numéro de l'IA en compétition (entre 0 et 9 actuellement)
-IA_ROUGE = 5  # Numéro de l'IA en compétition (entre 0 et 9 actuellement)
 
 ########################################################################
 
